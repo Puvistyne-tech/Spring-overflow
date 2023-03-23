5dec3e9e6f44627424c9aeee5fa4c0bae1254d42
Puvistyne Rajasegar <puvistien@gmail.com>
2023-03-23 14:20:34 +0100

    Tags with questions

--- src/main/java/uge/fr/ugeoverflow/service/DatabaseInitializer.java
+++ src/main/java/uge/fr/ugeoverflow/service/DatabaseInitializer.java
@@ -1,45 +1,44 @@
-//package uge.fr.ugeoverflow.service;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//import uge.fr.ugeoverflow.model.Address;
-//import uge.fr.ugeoverflow.model.User;
-//import uge.fr.ugeoverflow.repository.UserRepository;
-//import uge.fr.ugeoverflow.security.UserRole;
-//
-//import java.time.LocalDateTime;
-//
-//@Component
-//public class DatabaseInitializer implements CommandLineRunner {
-//
-//    private final TagService tagService;
-//
-//    private final UserRepository userRepository;
-//    private final AuthenticationService authenticationService;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    public DatabaseInitializer(TagService tagService, UserRepository userRepository, AuthenticationService authenticationService, PasswordEncoder passwordEncoder) {
-//        this.tagService = tagService;
-//        this.userRepository = userRepository;
-//        this.authenticationService = authenticationService;
-//        this.passwordEncoder = passwordEncoder;
-//    }
-//
-//    public void createAdmin() {
-//        if (userRepository.findByUsername("admin").isEmpty()) {
-//            var admin = new User("admin", "admin", "admin", "admin@admin.com", passwordEncoder.encode("admin"));
-//            admin.setRole(UserRole.ADMIN.name());
-//            admin.setToken(authenticationService.generateToken(admin));
-//            admin.setCreationTime(LocalDateTime.now());
-//            admin.setAddress(new Address("street", "city", "France", "12434"));
-//            userRepository.save(admin);
-//        }
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        tagService.initialize();
-//        createAdmin();
-//    }
-//}
+package uge.fr.ugeoverflow.service;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import uge.fr.ugeoverflow.model.Address;
+import uge.fr.ugeoverflow.model.User;
+import uge.fr.ugeoverflow.repository.UserRepository;
+import uge.fr.ugeoverflow.security.UserRole;
+
+import java.time.LocalDateTime;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final TagService tagService;
+
+    private final UserRepository userRepository;
+    private final AuthenticationService authenticationService;
+    private final PasswordEncoder passwordEncoder;
+
+    public DatabaseInitializer(TagService tagService, UserRepository userRepository, AuthenticationService authenticationService, PasswordEncoder passwordEncoder) {
+        this.tagService = tagService;
+        this.userRepository = userRepository;
+        this.authenticationService = authenticationService;
+        this.passwordEncoder = passwordEncoder;
+    }//
+    public void createAdmin() {
+        if (userRepository.findByUsername("admin").isEmpty()) {
+            var admin = new User("admin", "admin", "admin", "admin@admin.com", passwordEncoder.encode("admin"));
+            admin.setRole(UserRole.ADMIN.name());
+            admin.setToken(authenticationService.generateToken(admin));
+            admin.setCreationTime(LocalDateTime.now());
+            admin.setAddress(new Address("street", "city", "France", "12434"));
+            userRepository.save(admin);
+        }
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        tagService.initialize();
+        createAdmin();
+    }
+}
